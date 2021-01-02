50ca796b31bc05834e9eeec86024ead84730b6d2
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-02 12:18:14 +0600

    Fix resource route problem

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::resource('posts', [PostController::class]);
+Route::resource('posts', PostController::class);
